734b2efaa89f85a4f0cb0bdeb9abcb6d7b44b17a
kaxla <kaylas.email.address@gmail.com>
2014-01-13 16:54:13 -0800

    added cupcake test to welcome_page_spec.rb

--- spec/feature/welcome_page_spec.rb
+++ spec/feature/welcome_page_spec.rb
@@ -4,7 +4,12 @@ describe "A local web page" do
   it "has a welcome headline" do
     visit "file:///Users/kaylamorrison/Desktop/code-rails-chapter-1-master/public/index.html"
     page.text.must_include "Welcome aboard"
-  end
+	end
+ 
+  it "has cupcakes" do
+  	visit "file:///Users/kaylamorrison/Desktop/code-rails-chapter-1-master/public/index.html"
+  	page.text.must_include "cupcakes will inherit the earth"
+	end
 end
 
 
